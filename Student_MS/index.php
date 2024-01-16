58a3487f4a0e44f02e655050c4eb02df51cc2902
RosarioGelyn <133248629+Gelllllll@users.noreply.github.com>
2024-01-16 09:13:33 +0800

    Delete Student_MS directory

--- Student_MS/index.php
+++
@@ -1,9 +0,0 @@
-<?php include('./navbar.php'); ?>
-
-    <div class="m-5 p-2">
-        <h1 class="display-1"><b>successfully logout</h1>
-        <hr>
-        <p class="text-center p-3"> <a href="login.php"><b><h3>login now</h3></a></p>
-    </div>
-
-<?php include('./footer.php'); ?>
